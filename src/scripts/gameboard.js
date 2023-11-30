adad1bd059706e42c5b05419072eaba11ff2cb50
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-11-30 13:51:13 -0500

    Add tests and code for Ship Factory

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,3 +1,16 @@
-export function ShipFactory() {}
+/* eslint-disable no-plusplus */
+export function ShipFactory(length) {
+    let health = length;
+    const hit = () => --health;
+
+    let vertical = false;
+    const changeVertical = () => {
+        vertical = !vertical;
+    };
+    const isVertical = () => vertical;
+
+    const isSunk = () => health < 1;
+    return { length, hit, isSunk, changeVertical, isVertical };
+}
 
 export const example = '';
--- src/scripts/gameboard.test.js
+++ src/scripts/gameboard.test.js
@@ -7,4 +7,18 @@ describe('ShipFactory', () => {
     test('ship has length', () => {
         expect(ShipFactory(5).length).toBe(5);
     });
+    test('ship can change alignment', () => {
+        const ship = ShipFactory(5);
+        ship.changeVertical();
+        expect(ship.isVertical()).toBe(true);
+    });
+    test('ship hit reduces health', () => {
+        expect(ShipFactory(3).hit()).toBe(2);
+    });
+    test('ship can be sunk', () => {
+        const ship = ShipFactory(2);
+        ship.hit();
+        ship.hit();
+        expect(ship.isSunk()).toBe(true);
+    });
 });
