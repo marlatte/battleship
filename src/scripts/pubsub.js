9007db6c8d6e019a85046e991a2bb078df4e8c10
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-12-03 15:52:49 -0500

    Add pubsub

---
+++ src/scripts/pubsub.js
@@ -0,0 +1,34 @@
+export const PubSub = (() => {
+    const eventsList = {};
+    // { key: "triggerEvent", value: [ array of functions ] }
+
+    function subscribe(triggerEvent, subscriber) {
+        if (!eventsList[triggerEvent]) {
+            eventsList[triggerEvent] = [subscriber];
+        } else {
+            eventsList[triggerEvent].push(subscriber);
+        }
+        return {
+            unsubscribe: () => {
+                const functionList = eventsList[triggerEvent];
+                const i = functionList.indexOf(subscriber);
+                if (i > -1) {
+                    functionList.splice(i, 1);
+                }
+            },
+        };
+    }
+
+    function publish(triggerEvent, ...args) {
+        const functionList = eventsList[triggerEvent];
+        if (Array.isArray(functionList)) {
+            functionList.forEach((subscriber) => {
+                subscriber(...args);
+            });
+        }
+    }
+
+    return { subscribe, publish };
+})();
+
+export const E = {};
