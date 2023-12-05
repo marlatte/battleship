76604f6a0e8ba8a23871b729b196eac6525817b0
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-12-05 12:47:43 -0500

    Remove offense and defense boards

--- src/scripts/board.js
+++ src/scripts/board.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-param-reassign */
 
-import { currentPlayer } from '.';
-
 let counter = 0;
 
 export function ShipFactory(length) {
@@ -32,86 +30,6 @@ export function ShipFactory(length) {
         isVertical,
     };
 }
-
-export const DefenseBoards = (() => {
-    const player = {};
-    const computer = {};
-    const resetBoards = () => {
-        Object.keys(player).forEach((key) => {
-            delete player[key];
-        });
-        Object.keys(computer).forEach((key) => {
-            delete computer[key];
-        });
-
-        // Reassign methods
-        [player, computer].forEach((obj) => {
-            obj.placeShip = (ship, start) => {
-                const spots = [];
-                const multiplier = ship.isVertical() ? 10 : 1;
-                const end = start + multiplier * ship.length;
-                for (let i = start; i < end; i += multiplier) {
-                    const vertBool = i > 100;
-                    const horizBool = !(i % 10) || vertBool;
-                    const overlapAdj = [0, -1, 1, -10, 10]
-                        .map((num) => obj[i + num])
-                        .filter(Boolean);
-                    if (
-                        (ship.isVertical()
-                            ? vertBool
-                            : horizBool && i !== start) ||
-                        overlapAdj.length
-                    ) {
-                        return;
-                    }
-                    spots.push(i);
-                }
-                spots.forEach((i) => {
-                    obj[i] = ship;
-                });
-            };
-            obj.getShipsLeft = () =>
-                [
-                    ...new Set(Object.values(obj).filter((item) => item.id)),
-                ].filter((ship) => ship.isAfloat()).length;
-        });
-    };
-    resetBoards();
-    return {
-        player,
-        computer,
-        resetBoards,
-    };
-})();
-
-export const OffenseBoards = (() => {
-    const player = [];
-    const computer = [];
-
-    const resetBoards = () => {
-        for (let i = 0; i < 100; i += 1) {
-            player[i] = 0;
-            computer[i] = 0;
-        }
-    };
-
-    [player, computer].forEach((arr) => {
-        arr.receiveAttack = (square) => {
-            const ship = DefenseBoards[currentPlayer][square];
-            if (ship) {
-                ship.hit();
-                arr[square] = 2;
-                return ship.isAfloat();
-            }
-            arr[square] = 1;
-            return undefined;
-        };
-    });
-
-    resetBoards();
-
-    return { player, computer, resetBoards };
-})();
 
 export const Square = () => {
     let attacked = 0;
