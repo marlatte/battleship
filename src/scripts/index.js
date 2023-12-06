4293518420a6fa9c91d30163a33394169ba30660
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-12-06 12:03:57 -0500

    Build basic grids in DOM

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import { Game, Player } from './game-controller';
 import '../styles/style.css';
+import { elFactory, htmlFactory } from './helpers';
 
 const TestButtons = (() => {
     const startBtn = document.querySelector('#start-btn');
@@ -30,6 +31,32 @@ const TestButtons = (() => {
     });
 })();
 
-const shipsBoard = document.querySelector('.board ships');
+const boardsSection = document.querySelector('section.boards');
 
-const attacksBoard = document.querySelector('.board attacks');
+function buildBoards() {
+    const shipsBoard = elFactory('div', { classList: 'board ships' }, []);
+    const attacksBoard = elFactory('div', { classList: 'board attacks' }, []);
+
+    for (let i = 0; i < 100; i += 1) {
+        shipsBoard.children.push(
+            elFactory('button', {
+                classList: 'square',
+                dataset: { coord: `ship-${i}` },
+            })
+        );
+        attacksBoard.children.push(
+            elFactory('button', {
+                classList: 'square',
+                dataset: { coord: `attack-${i}` },
+            })
+        );
+    }
+
+    const container = elFactory('div', { classList: 'container' }, [
+        shipsBoard,
+        attacksBoard,
+    ]);
+    boardsSection.appendChild(htmlFactory(container));
+}
+
+buildBoards();
