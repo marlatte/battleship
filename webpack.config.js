979c40ca6b69eba65ee26a5f0f3d88cdec6e8f37
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-12-12 02:46:42 -0500

    Production build 1.0

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-const devMode = true;
+const devMode = false;
 
 module.exports = {
-  mode: devMode ? 'development' : 'production',
-  devtool: devMode ? 'inline-source-map' : 'source-map',
-  entry: { index: './src/scripts/index.js' },
-  output: {
-    filename: devMode ? '[name].bundle.js' : 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
+    mode: devMode ? 'development' : 'production',
+    devtool: devMode ? 'inline-source-map' : 'source-map',
+    entry: { index: './src/scripts/index.js' },
+    output: {
+        filename: devMode ? '[name].bundle.js' : 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
         ],
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [new CssMinimizerPlugin()],
-  },
+    },
+    optimization: {
+        minimizer: [new CssMinimizerPlugin()],
+    },
 };
